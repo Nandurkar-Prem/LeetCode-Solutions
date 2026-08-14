45b7455f97f365385bdd4a35d1fdfac307a7098b
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-08-14 14:42:38 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (94.88%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        //Here we have to use the Sliding Window pattern
         int length = 0;
         int left = 0;
-        int[] charFrequency = new int[26];//It's a frequency array
+        int[] charFrequency = new int[26];
 
         for(int right=0;right<s.length();right++){
             charFrequency[s.charAt(right) - 'a']++;
