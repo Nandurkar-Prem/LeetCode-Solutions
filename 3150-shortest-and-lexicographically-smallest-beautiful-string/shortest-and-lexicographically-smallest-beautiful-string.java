266fb4de84993199ac4da5d3b6fb05c0b95278db
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-08-28 02:28:54 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (77.80%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int left = 0;
-        int countOnes = 0;
         int minLength = Integer.MAX_VALUE;
         String result = "";
+        int countOnes = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            if (s.charAt(right) == '1') {
+        for(int right=0;right<s.length();right++){
+            if(s.charAt(right) == '1'){
                 countOnes++;
             }
 
-            while (countOnes == k) {
-                int currentLength = right - left + 1;
-                String currentSubstring = s.substring(left, right + 1);
+            while(countOnes == k){
+                int currentLength = right-left+1;
+                String currentString = s.substring(left, right+1);
 
-                if (currentLength < minLength) {
+                if(currentLength < minLength){
                     minLength = currentLength;
-                    result = currentSubstring;
-                } 
-                else if (currentLength == minLength) {
-                    if (currentSubstring.compareTo(result) < 0) {
-                        result = currentSubstring;
+                    result = currentString;
+                }else if(currentLength == minLength){
+                    if(currentString.compareTo(result) < 0){
+                        result = currentString;
                     }
                 }
-
-                if (s.charAt(left) == '1') {
+                if(s.charAt(left) == '1'){
                     countOnes--;
                 }
                 left++;
