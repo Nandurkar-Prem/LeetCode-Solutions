6400bbc377ee98d1bd6b7a1d39ed7648b29735c8
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-08-31 14:25:13 +0530

    Time: 5 ms (40.73%) | Memory: 106.1 MB (55.32%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -13,37 +13,37 @@ class Solution {
         if(head == null || head.next == null || head.next.next == null){
             return new int[] {-1,-1};
         }
+
         int firstCriticalIndex = -1;
         int prevCriticalIndex = -1;
 
+        int minDistance = Integer.MAX_VALUE;
+        int currIndex = 1;
+
         ListNode prev = head;
         ListNode curr = head.next;
-        int currentIndex = 1;
-        int minDistance = Integer.MAX_VALUE;
 
         while(curr.next != null){
             ListNode nextNode = curr.next;
-
             boolean maxima = curr.val > prev.val && curr.val > nextNode.val;
             boolean minima = curr.val < prev.val && curr.val < nextNode.val;
-            
+
             if(maxima || minima){
                 if(firstCriticalIndex == -1){
-                    firstCriticalIndex = currentIndex;
+                    firstCriticalIndex = currIndex;
                 }else{
-                    minDistance = Math.min(minDistance, currentIndex-prevCriticalIndex);
+                    minDistance = Math.min(minDistance, currIndex-prevCriticalIndex);
                 }
-                prevCriticalIndex = currentIndex;
+                prevCriticalIndex = currIndex;
             }
             prev = curr;
             curr = nextNode;
-            currentIndex++;
+            currIndex++;
         }
 
         if(firstCriticalIndex == prevCriticalIndex){
             return new int[] {-1,-1};
         }
-
         int maxDistance = prevCriticalIndex - firstCriticalIndex;
         return new int[] {minDistance, maxDistance};
     }
