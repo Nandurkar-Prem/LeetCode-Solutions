b6921fdda1308d4dbd744a887e99023c9180aafd
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-09-06 22:55:02 +0530

    Time: 2 ms (91.78%) | Memory: 47.7 MB (76.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int start = 0;
-        int len = nums.length;
-
-        // Step 1: Overwrite forward with non-zero elements
-        for (int i = 0; i < len; i++) {
-            if (nums[i] != 0) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0){
                 nums[start] = nums[i];
                 start++;
-            }
+            } 
         }
-
-        // Step 2: Fill the rest of the array with zeroes
-        while (start < len) {
-            nums[start] = 0;
-            start++;
+        while(start < nums.length){
+           nums[start++] = 0;
         }
     }
 }
