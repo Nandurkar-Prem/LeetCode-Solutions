8f8b0bd0592e1a2528a32bf11855f4b9aa9f8865
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-09-24 00:33:00 +0530

    Time: 4 ms (83.76%) | Memory: 44.4 MB (65.87%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,21 +4,17 @@ class Solution {
             return false;
         }
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] charCounts = new int[26];
 
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+            charCounts[s.charAt(i) - 'a']++;
         }
 
-        for(int j=0;j<t.length();j++){
-            char ch = t.charAt(j);
-
-            if(!map.containsKey(ch) || map.get(ch) == 0){
+        for(int i=0;i<t.length();i++){
+            int index = t.charAt(i) - 'a';
+            if(--charCounts[index] < 0){
                 return false;
             }
-
-            map.put(ch, map.get(ch) - 1);
         }
         return true;
     }
