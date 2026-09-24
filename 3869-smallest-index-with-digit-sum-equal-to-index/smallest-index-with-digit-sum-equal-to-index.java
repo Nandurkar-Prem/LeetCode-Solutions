57f504c1db7cfd701431d8339205602d07b6dcfa
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-09-24 12:02:37 +0530

    Time: 1 ms (99.69%) | Memory: 46.2 MB (5.14%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -18,10 +18,9 @@ class Solution {
     static int digitSum(int n){
         int sum = 0;
         while(n > 0){
-            int mod = n % 10;
-            sum += mod;
+            sum += n % 10;
             n /= 10;
         }
-        return sum;
+        return sum; 
     }
 }
